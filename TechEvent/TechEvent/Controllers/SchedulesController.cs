f5ae37326220f18ededa3011cfacef5024963223
ginaisvor <ginaisvor@yahoo.com>
2019-05-13 19:25:28 +0300

    proiectul final

---
+++ TechEvent/TechEvent/Controllers/SchedulesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using TechEvent.Domain.Entities;
+using TechEvent.Service;
+
+namespace TechEvent.Web.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        private readonly IScheduleService service;
+        private readonly IEditionService editionService;
+
+        public SchedulesController(IScheduleService service, IEditionService editionService)
+        {
+            this.service = service;
+            this.editionService = editionService;
+        }
+
+        public IActionResult Index(int year, [FromServices]IRoomService roomService)
+        {
+            int editionId = Edition.Years.IndexOf(year);
+            if (editionId <= 0)
+                return NotFound("We don't have this edition");
+            var schedules = service.GetSchedules(editionId);
+            ViewBag.Rooms = roomService.GetAll(editionId, false);
+            ViewBag.AllowToEdit = editionService.AllowToModify(year);
+            ViewBag.Year = year;
+
+            return View(schedules);
+        }
+    }
+}
---
+++ TechEvent/TechEvent/TagHelpers/AllEditionsTagHelper.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechEvent.Domain.Entities;
+using TechEvent.Service;
+
+namespace TechEvent.Web.TagHelpers
+{
+    [HtmlTargetElement("all-editions")]
+    public class AllEditionsTagHelper : TagHelper
+    {
+        private readonly IEditionService service;
+
+        public AllEditionsTagHelper(IEditionService service)
+        {
+            this.service = service;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("id", "CurrentViewEdition");
+            output.Attributes.SetAttribute("name", "CurrentViewEdition");
+
+            var editions = service.GetAll().OrderBy(e => e.Year);
+            var current = Edition.CurrentEdition ;
+
+            foreach (var edition in editions)
+            {
+                TagBuilder option = new TagBuilder("span")
+                {
+                    TagRenderMode = TagRenderMode.Normal
+                };
+
+                if (edition.Id == current)
+                {
+                    option.AddCssClass("active");
+                }
+               // < a class="dropdown-item nav-link text-dark" asp-area="" asp-controller="Speakers" asp-action="Index">Speakers</a>
+                 option.InnerHtml.AppendHtml("<a class='dropdown - item nav - link text - dark' href='/" + edition.Year+"/Home/Index'"+">"+edition.Tagline+"</a>");
+
+                output.Content.AppendHtml(option);
+            }
+        }
+    }
+}
